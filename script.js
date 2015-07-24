95038994350fab69a2dc3923e44e9f90646f4db8
Juvenal Manikan <jmanikan@users.noreply.github.com>
2015-07-24 13:42:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+var template = function(text) {
+  return '<span class="glyphicon glyphicon-minus"></span>';
+};
+
 var main = function() {
-  
+  $('.day').click(function() {
+    // console.log("Goy");
+    $('.hourly').toggle();
+  });
+
+  // $('.day').click(function() {
+  //   $(this).find('.glyphicon-plus').remove();
+  // });
+
+  $('.day').on('click', '.weekday', function() {
+    $(this).find('span').toggleClass('glyphicon-minus');
+  });
+
+
 };
 
 $(document).ready(main);
